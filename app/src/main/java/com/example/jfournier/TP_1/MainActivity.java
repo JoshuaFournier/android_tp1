c841675fdf2d72f7a6bebe7a7ac5240c58b5ca0c
jfournier <jfournie6@gmail.com>
2017-11-24 08:20:56 +0100

    Android_TP : 1er commit - Exercice OK

---
+++ app/src/main/java/com/example/jfournier/TP_1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jfournier.TP_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.jfournier.TP_1.R.layout.activity_main);
+    }
+}
